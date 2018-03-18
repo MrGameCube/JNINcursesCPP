2619bcc7a4aa2cb8f171816498aeb73fc4cbcc2b
MrGameCube <mrgamecube@muffinanddonut.de>
2018-03-18 16:00:23 +0100

    Temporarily removed wide character support

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
-g++ -lncursesw -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -fPIC -shared -o libjnincurses.so JniNcurses.cpp
+g++ -lncurses -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -fPIC -shared -o libjnincurses.so JniNcurses.cpp
 
