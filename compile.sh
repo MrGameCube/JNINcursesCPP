80dec977e0f2cd8002a831116d9d1dbc7f013b63
MrGameCube <mrgamecube@muffinanddonut.de>
2018-03-18 15:58:31 +0100

    Added dynamic jdk Path

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
-g++ -lncursesw -I/usr/lib/jvm/java-9-openjdk/include/linux/ -I/usr/lib/jvm/java-9-openjdk/include/ -fPIC -shared -o libjnincurses.so JniNcurses.cpp
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+g++ -lncursesw -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -fPIC -shared -o libjnincurses.so JniNcurses.cpp
 
