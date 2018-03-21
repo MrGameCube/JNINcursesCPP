839596749af0c51b94cb63d9f1219ff3547cfbd5
MrGameCube <mrgamecube@muffinanddonut.de>
2018-03-21 18:19:15 +0100

    Readded wide character support

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
-g++ -lncurses -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -fPIC -shared -o libjnincurses.so JniNcurses.cpp
+g++ -lncursesw -I$JAVA_HOME/include/linux/ 
+-I$JAVA_HOME/include/ -fPIC -shared -o libjnincurses.so JniNcurses.cpp
 
